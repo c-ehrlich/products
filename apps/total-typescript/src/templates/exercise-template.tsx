142c5cd6a979086ddcc65a451b46c5a2b6e269fe
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2023-02-25 23:04:04 +0100

    dont need this

--- apps/total-typescript/src/templates/exercise-template.tsx
+++ apps/total-typescript/src/templates/exercise-template.tsx
@@ -105,7 +105,6 @@ const ExerciseTemplate: React.FC<{
                 <button
                   className="bg-red-500 p-4"
                   onClick={() => {
-                    console.log('videoProvider onModuleEnded onSettled')
                     utils.moduleProgress.bySlug.invalidate({
                       slug: module.slug.current,
                     })
